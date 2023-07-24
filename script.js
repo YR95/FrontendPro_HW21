93f64fd6ae2a7035a0f31a6690d4a3a5fc07695e
yuliia <pustovayajulia@gmail.com>
2023-07-24 19:30:16 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function deepClone(array) {
+    const newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        if (Array.isArray(array[i])) {
+            newArray.push(deepClone(array[i]))
+        } else {
+
+            newArray.push(array[i])
+        }
+    }
+    console.log(newArray)
+    return newArray;
+
+}
+
+const test = [
+    [
+        ['some text', true, [1, 2, 3, 4, [10, 20]]],
+    ],
+    [
+        [1, 2, 3, [100, 200]],
+        ['name', 'age']
+    ],
+];
+deepClone(test);
